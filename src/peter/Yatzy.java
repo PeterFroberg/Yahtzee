4d051340e88237e0ae739b9223fbcdc2a4f159e2
Peter Fröberg <peter.froberg@stenhamra.se>
2020-05-23 22:22:57 +0200

    invite player - done join - done Start game and remake messageQueues - done Game Play - done Code cleaning - in progress Panels remake - Done

--- src/peter/Yatzy.java
+++ src/peter/Yatzy.java
@@ -43,28 +43,11 @@ public class Yatzy extends JPanel implements Runnable {
     private final BufferedReader socketReader;
     private final PrintWriter socketWriter;
 
-
-    //private static JMenuBar menuBar = new JMenuBar();
-
-    /**
-     * Create GUI components for the application
-     */
-
-    //Menu items
-//    private JMenuItem menuItemCreateNewPlayer = new JMenuItem("Create new player");
-//    private JMenuItem menuItemInvitePlayers = new JMenuItem("Invite players");
-//    private JMenuItem menuItemLogin = new JMenuItem("Login");
-//    private JMenuItem menuItemJoinGame = new JMenuItem("Join game");
-//    //private JMenuItem menuItemScoreBoard = new JMenuItem("My Scoreboard");
-//    private JMenuItem menuItemExit = new JMenuItem("Exit");
-
     public Yatzy(BufferedReader socketReader, PrintWriter socketWriter) {
 
         this.socketReader = socketReader;
         this.socketWriter = socketWriter;
 
-        //Create Menu
-        //menuBar = createMenuBar();
         //Set Layout for the frame
         setLayout(new BorderLayout());
 
@@ -93,12 +76,7 @@ public class Yatzy extends JPanel implements Runnable {
      * @param value - true/false
      */
     private void enableMenuOptions(boolean value) {
-        //menuItemScoreBoard.setEnabled(value);
         menuBar.enabelMenuOptions(value);
-//        menuItemInvitePlayers.setEnabled(value);
-//        menuItemJoinGame.setEnabled(value);
-
-        //buttonSendChat.setEnabled(value);
     }
 
     /**
@@ -462,47 +440,6 @@ public class Yatzy extends JPanel implements Runnable {
                 break;
         }
     }
-
-    /**
-     * creates the menubar and populates it with swing components
-     *
-     * @return - returns the created menubar
-     */
-//    private JMenuBar createMenuBar() {
-//        menuBar = new JMenuBar();
-//        JMenu menu = new JMenu("Menu");
-//        menu.setMnemonic(KeyEvent.VK_P);
-//        menuBar.add(menu);
-//
-//        menuItemCreateNewPlayer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.ALT_DOWN_MASK));
-//        menuItemCreateNewPlayer.addActionListener(actionEvent -> createNewUser());
-//
-//        menuItemLogin.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.ALT_DOWN_MASK));
-//        menuItemLogin.addActionListener(actionEvent -> loginplayer());
-//
-//        menuItemInvitePlayers.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, InputEvent.ALT_DOWN_MASK));
-//        menuItemInvitePlayers.setEnabled(false);
-//        menuItemInvitePlayers.addActionListener(actionEvent -> invitePlayer());
-//
-//        menuItemJoinGame.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_J, InputEvent.ALT_DOWN_MASK));
-//        menuItemJoinGame.setEnabled(false);
-//        menuItemJoinGame.addActionListener(actionEvent -> joinGame());
-//
-////        menuItemScoreBoard.setAccelerator((KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.ALT_DOWN_MASK)));
-////        menuItemScoreBoard.setEnabled(false);
-//
-//        menuItemExit.setAccelerator((KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.ALT_DOWN_MASK)));
-//        menuItemExit.addActionListener(actionEvent -> System.exit(0));
-//
-//        menu.add(menuItemCreateNewPlayer);
-//        menu.add(menuItemLogin);
-//        menu.add(menuItemInvitePlayers);
-//        menu.add(menuItemJoinGame);
-//        //menu.add(menuItemScoreBoard);
-//        menu.add(menuItemExit);
-//
-//        return menuBar;
-//    }
 
     /**
      * Setup communication with the server, Starts the GUI
